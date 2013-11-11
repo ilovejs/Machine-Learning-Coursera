9c8bb6452508858e88270cdf390f09513da87ebe
Larry ~ Xiao Di <xiaodi@sjtu.edu.cn>
2013-11-11 08:56:05 +0800

    experiment with lamda

--- mlclass-ex2/ex2_reg.m
+++ mlclass-ex2/ex2_reg.m
@@ -87,6 +87,20 @@ pause;
 initial_theta = zeros(size(X, 2), 1);
 
 % Set regularization parameter lambda to 1 (you should vary this)
+
+% Notice the changes in the decision boundary as you vary λ. With a small
+% λ, you should find that the classifier gets almost every training example
+% correct, but draws a very complicated boundary, thus overfitting the data
+% (Figure 5). This is not a good decision boundary: for example, it predicts
+% that a point at x = (−0.25, 1.5) is accepted (y = 1), which seems to be an
+% incorrect decision given the training set.
+
+% With a larger λ, you should see a plot that shows an simpler decision
+% boundary which still separates the positives and negatives fairly well. How-
+% ever, if λ is set to too high a value, you will not get a good fit and the decision
+% boundary will not follow the data so well, thus underfitting the data (Figure
+% 6).
+
 lambda = 1;
 
 % Set Options
